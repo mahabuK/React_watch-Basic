2861a3863d8c28563e61257bbb79590705786901
mahabu@kamy <mohammed.mohammed@kamy.no>
2022-10-13 22:28:58 +0200

    update acr sh

--- k8s/acr.sh
+++ k8s/acr.sh
@@ -6,10 +6,10 @@ ACR_RESOURCE_GROUP=
 ACR_NAME=
 
 # Get the id of the service principal configured for AKS
-CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv)
+CLIENT_ID=$(az aks show --resource-group AzurekamyKubernetProject --name KamyClusterMonday --query "servicePrincipalProfile.clientId" --output tsv)
 
 # Get the ACR registry resource id
-ACR_ID=$(az acr show --name $ACR_NAME --resource-group $ACR_RESOURCE_GROUP --query "id" --output tsv)
+ACR_ID=$(az acr show --name kamymandagacr02 --resource-group AzurekamyKubernetProject --query "id" --output tsv)
 
 # Create role assignment
-az role assignment create --assignee $CLIENT_ID --role acrpull --scope $ACR_ID
+az role assignment create --assignee 0059b125-455b-475b-a19c-bbef96325d52 --role acrpull --scope /subscriptions/2472e10a-2055-41f0-8b6a-991f14748492/resourceGroups/AzurekamyKubernetProject/providers/Microsoft.ContainerRegistry/registries/kamymandagacr02
